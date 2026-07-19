ea6a1cb2e98d270c94efff0a8df60333df88bb82
gunmind <Leoleonelo@outlook.com>
2026-07-19 12:41:13 -0600

    Nuevo archivo, 3ra interacción

---
+++ Calculo 1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 30
+c = 30 * 10 * 5
+
+print('resultado=', c)
+
+## cambiado el factor 102 según requerimiento del negocio
+## cambiado el factor 110 según junta control
+
+d = c * 110
+print('resultado total=', d)
